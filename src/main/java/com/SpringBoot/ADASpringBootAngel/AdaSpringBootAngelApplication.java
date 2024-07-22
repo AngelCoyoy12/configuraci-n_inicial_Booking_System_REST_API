059ad8f45cf151a2ce1a13d85fdc61ae4d4273a3
Angel Coyoy <furioso7s12@gmail.com>
2024-07-22 08:37:22 -0600

    First commit

---
+++ src/main/java/com/SpringBoot/ADASpringBootAngel/AdaSpringBootAngelApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.ADASpringBootAngel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaSpringBootAngelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaSpringBootAngelApplication.class, args);
+	}
+
+}
